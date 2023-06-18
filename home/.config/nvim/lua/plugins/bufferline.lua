464029133576ac5394e25bfc7fdf86341f6d4f35
Nozomu Yamazaki <46639706+nozomu-y@users.noreply.github.com>
2023-06-18 11:09:25 +0900

    Update: change buffer close icon

--- home/.config/nvim/lua/plugins/bufferline.lua
+++ home/.config/nvim/lua/plugins/bufferline.lua
@@ -1,10 +1,12 @@
 local status, bufferline = pcall(require, "bufferline")
 if not status then
-    return
+	return
 end
 
 bufferline.setup({
-    options = {
-        offsets = { { filetype = "NvimTree", text = "NvimTree", padding = 1 } },
-    },
+	options = {
+		offsets = { { filetype = "NvimTree", text = "NvimTree", padding = 1 } },
+		-- buffer_close_icon = "",
+		buffer_close_icon = "",
+	},
 })
